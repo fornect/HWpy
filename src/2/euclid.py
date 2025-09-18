f44a397c29a0f36a29ef011bfb45ede9c83aabd4
Глеб Мельник <melnikgleb113@gmail.com>
2025-09-18 18:23:50 +0300

    Add Extended Euclidean Algorithm

--- src/2/euclid.py
+++ src/2/euclid.py
@@ -1,5 +1,11 @@
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-print(52)
+def gcdex(a, b):
+    if b==0: #если b = 0 то НОД = a
+        return a, 1, 0 #вывод 1)это НОД 2,3)сколько a и b нужно для того чтобы получить НОД
+    else:
+        d, x, y = gcdex(b,a%b) 
+        return d,y,x-y*(a//b)
+
+
+a = int(input()) 
+b = int(input())
+print(gcdex(a,b))
