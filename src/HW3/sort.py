ef51a845b7d5d253d91dc7dcbd80d41ba61367da
Глеб Мельник <melnikgleb113@gmail.com>
2025-12-19 19:03:02 +0300

    Add bubble_sort

--- src/HW3/sort.py
+++ src/HW3/sort.py
@@ -1,10 +1,13 @@
-def sortirovka(mas):
-    
-    for i in range(len(mas)):
-        while mas[i] > mas[i-1] and i != 0:
-            mas[i], mas[i-1] = mas[i-1], mas[i]
-            i -= 1
-    return mas
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        swapped = False
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
 
 
-print(sortirovka([1,2,3,4,9,2,4,0,-10,35,-90]))
+print(bubble_sort([1, 2, 3, 0, 9, 2, 4, -10, 35, -90]))
